66b195c8138a0b03f264af2c5028fff439f3ed97
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2020-09-23 12:08:59 -0500

    Added example3 about bigO notation

---
+++ BigO/example3.ts
@@ -0,0 +1,26 @@
+/**
+ * Example 3, Pag 46.
+ * Calculate the compexity about the algoritm.
+ */
+
+const printUnOrderedPairs = (data: number[][] = []) => {
+  for (let i = 0; i < data.length; i++) {
+    console.group(`row:${i}`);
+    for (let j = i + 1; j < data.length; j++) {
+      console.log(`(${i},${j})`);
+    }
+    console.groupEnd();
+  }
+};
+
+const data = [
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+  [1, 2, 3, 4, 5, 6, 7, 8],
+];
+
+printUnOrderedPairs(data);
